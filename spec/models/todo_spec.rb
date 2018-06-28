318b9d59c4e1961afc9a835d2bb0631bf42d17f8
JordanaN <jordanasnogueira@gmail.com>
2018-06-28 18:13:31 -0300

    primeiro teste com rails

---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+# RSpec.describe Todo, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+RSpec.describe Todo, type: :model do
+  it { should have_many(:items).dependent(:destroy) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
+end
